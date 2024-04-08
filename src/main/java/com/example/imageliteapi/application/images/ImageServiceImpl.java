690f77e88268ac482ef25e10c625244afe64c424
Eriklps <erikdslopes@gmail.com>
2024-04-08 17:16:28 -0300

    Implemented URL creation to access the image for rendering

--- src/main/java/com/example/imageliteapi/application/images/ImageServiceImpl.java
+++ src/main/java/com/example/imageliteapi/application/images/ImageServiceImpl.java
@@ -1,19 +1,35 @@
 package com.example.imageliteapi.application.images;
 
 import com.example.imageliteapi.domain.entity.Image;
+import com.example.imageliteapi.domain.enums.ImageExtension;
 import com.example.imageliteapi.domain.service.ImageService;
 import com.example.imageliteapi.infra.repository.ImageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class ImageServiceImpl implements ImageService {
+
     private final ImageRepository repository;
+
     @Override
     @Transactional
     public Image save(Image image) {
         return repository.save(image);
+    }
+
+    @Override
+    public Optional<Image> getById(String id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<Image> search(ImageExtension extension, String query) {
+        return null;
     }
 }
--- src/main/java/com/example/imageliteapi/domain/service/ImageService.java
+++ src/main/java/com/example/imageliteapi/domain/service/ImageService.java
@@ -1,7 +1,15 @@
 package com.example.imageliteapi.domain.service;
 
 import com.example.imageliteapi.domain.entity.Image;
+import com.example.imageliteapi.domain.enums.ImageExtension;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ImageService {
     Image save(Image image);
+
+    Optional<Image> getById(String id);
+
+    List<Image> search(ImageExtension extension, String query);
 }
