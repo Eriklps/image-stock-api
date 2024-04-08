04c1093cbf1c951f9de3c3c16672f710362218cc
Eriklps <erikdslopes@gmail.com>
2024-04-08 14:45:53 -0300

    Implemented save image through the API

--- src/main/java/com/example/imageliteapi/domain/enums/ImageExtension.java
+++ src/main/java/com/example/imageliteapi/domain/enums/ImageExtension.java
@@ -6,8 +6,23 @@ import org.springframework.http.MediaType;
 import java.util.Arrays;
 
 public enum ImageExtension {
-    JPEG,
-    JPG,
-    PNG,
-    GIF;
+
+    PNG(MediaType.IMAGE_PNG),
+    GIF(MediaType.IMAGE_GIF),
+    JPEG(MediaType.IMAGE_JPEG);
+
+    @Getter
+    private MediaType mediaType;
+
+    ImageExtension(MediaType mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public static ImageExtension valueOf(MediaType mediaType){
+        return Arrays.stream(values())
+                .filter(ie -> ie.mediaType.equals(mediaType))
+                .findFirst()
+                .orElse(null);
+    }
+
 }
